14124217daf6e6a1df938eca6e14c56bd5e799fe
viruss001 <122778544+viruss001@users.noreply.github.com>
2024-02-24 15:18:50 +0530

    here I upload my project

---
+++ 01project/script.js
@@ -0,0 +1,15 @@
+let display= document.querySelectorAll(".num")
+let interval = 5000
+display.forEach((i)=>{
+    let start = 0
+    let end = parseInt(i.getAttribute("data-val"))
+    let dur = Math.floor(interval/end)
+    let counter = setInterval(()=>{
+        start+=1
+        i.textContent=start
+        if(start ==end){
+            clearInterval(counter)
+        }
+
+    },dur)
+})
